9b2e98182d6c4f1bfd5e2ab10d35715292d4f655
dedoxd2 <dedo.xd33@yahoo.com>
2023-10-01 14:17:04 +0300

    Used created Form , Models, Used API , Print Data

--- Weather_API/theweather/theweather/settings.py
+++ Weather_API/theweather/theweather/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 
+from pathlib import Path
+from decouple import config, Csv
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%=hcm_=6p^ezf39$svtx%uwwnk=cnu6yjvc@gh^hfo9i55xtp='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
--- Weather_API/theweather/weather_api/views.py
+++ Weather_API/theweather/weather_api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
-
+import requests
+from .models import City
+from .forms import CityForm
 # Create your views here.
+
+
+def index(request):  # sourcery skip: move-assign
+
+    url = "http://api.openweathermap.org./data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q="
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    form = CityForm()
+    cities = City.objects.all()
+    weather_data = []
+    if cities:
+        for city in cities:
+            data_url = url + str(city)
+            try:
+                response = requests.get(data_url).json()
+                city_weather = {
+                    'city': city.name,
+                    'temperature': response['main']['temp'],
+                    'description': response['weather'][0]['description'],
+                    'icon': response['weather'][0]['icon'],
+                }
+
+                weather_data.append(city_weather)
+            except Exception:
+                print("There is No Country With This Name ")
+
+    return render(request, 'weather.html', {'weather_data': weather_data, 'form': form})
